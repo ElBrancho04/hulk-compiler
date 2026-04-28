4cb18ab77c1d0a1e18298ed6ac681a74d7d399c2
abraham <maharbazehcnas@gmail.com>
2026-04-27 20:48:41 -0400

    add if for and while in AST

--- include/ast.hpp
+++ include/ast.hpp
@@ -9,6 +9,16 @@
 class NumberLiteral;
 class StringLiteral;
 class BoolLiteral;
+class BinaryExpr;
+class UnaryExpr;
+class BlockExpr;
+class VarRef;
+class AssignExpr;
+class LetBinding;
+class LetExpr;
+class IfExpr;
+class WhileExpr;
+class ForExpr;
 
 template <typename T>
 class Visitor {
@@ -16,6 +26,16 @@ public:
     virtual T visit(NumberLiteral& node) = 0;
     virtual T visit(StringLiteral& node) = 0;
     virtual T visit(BoolLiteral& node) = 0;
+    virtual T visit(BinaryExpr& node) = 0;
+    virtual T visit(UnaryExpr& node) = 0;
+    virtual T visit(BlockExpr& node) = 0;
+    virtual T visit(VarRef& node) = 0;
+    virtual T visit(AssignExpr& node) = 0;
+    virtual T visit(LetBinding& node) = 0;
+    virtual T visit(LetExpr& node) = 0;
+    virtual T visit(IfExpr& node) = 0;
+    virtual T visit(WhileExpr& node) = 0;
+    virtual T visit(ForExpr& node) = 0;
     virtual ~Visitor() = default;
 };
 
@@ -62,6 +82,48 @@ public:
     void accept(Visitor<void>& visitor) override {
         visitor.visit(*this);
     }
+};
+
+struct IfBranch {
+    std::unique_ptr<Expr> condition;
+    std::unique_ptr<Expr> body;
+
+    IfBranch(std::unique_ptr<Expr> cond, std::unique_ptr<Expr> b)
+        : condition(std::move(cond)), body(std::move(b)) {}
+};
+
+class IfExpr : public Expr {
+public:
+    std::vector<IfBranch> branches;
+    std::unique_ptr<Expr> else_body;
+
+    IfExpr(std::vector<IfBranch> branches, std::unique_ptr<Expr> else_body, int line)
+        : Expr(line), branches(std::move(branches)), else_body(std::move(else_body)) {}
+
+    void accept(Visitor<void>& visitor) override { visitor.visit(*this); }
+};
+
+class WhileExpr : public Expr {
+public:
+    std::unique_ptr<Expr> condition;
+    std::unique_ptr<Expr> body;
+
+    WhileExpr(std::unique_ptr<Expr> condition, std::unique_ptr<Expr> body, int line)
+        : Expr(line), condition(std::move(condition)), body(std::move(body)) {}
+
+    void accept(Visitor<void>& visitor) override { visitor.visit(*this); }
+};
+
+class ForExpr : public Expr {
+public:
+    std::string variable_name;
+    std::unique_ptr<Expr> iterable;
+    std::unique_ptr<Expr> body;
+
+    ForExpr(std::string var_name, std::unique_ptr<Expr> iterable, std::unique_ptr<Expr> body, int line)
+        : Expr(line), variable_name(std::move(var_name)), iterable(std::move(iterable)), body(std::move(body)) {}
+
+    void accept(Visitor<void>& visitor) override { visitor.visit(*this); }
 };
 
 #endif
