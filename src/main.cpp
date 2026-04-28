e466cb7bc572ed41f7a6d6c3c0ca92a41e877eb0
abraham <maharbazehcnas@gmail.com>
2026-04-28 13:17:18 -0400

    setup flex skeleton

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "ast.hpp"
+
+extern int yylex();
+extern int line_number;
 
 int main() {
-    std::cout << "HULK Compiler - Initialized" << std::endl;
+    std::cout << "HULK Compiler - Testing Lexer Compilation" << std::endl;
     return 0;
 }
